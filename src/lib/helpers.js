6708024bf2617d65292ccfe254a505e384572b07
Una Kamilla <unakamilla@gmail.com>
2019-11-22 09:16:40 +0000

    bleh

--- src/lib/helpers.js
+++ src/lib/helpers.js
@@ -38,3 +38,33 @@ export function el(name, ...children) {
 export function randomNumber(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
+// velja random dagsetningu
+export function randomDate() {
+  let day;
+  let month;
+  let year = randomNumber(1995, 2019);
+  if (year == 1995) { // first available date: 1995-06-16
+    month = randomNumber(6,12);
+    if (month == 6) {
+      day = randomNumber(16,30);
+    }
+  } else {
+    month = randomNumber(1,12);
+    switch (month) {
+      case 2: // february
+        day = randomNumber(1,28);
+        break;
+      case 4: // april
+      case 6: // june
+      case 9: // september
+      case 11: // november
+        day = randomNumber(1,30);
+        break;
+      default: // other months
+        day = randomNumber(1,31);
+    }
+  }
+  let date = ´${year}-${month}-${day}´;
+  return date;
+}
